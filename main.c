5c598712dcaa5bf1f96d95d0a096240faddde534
Pavlos Fragkiadoulakis <pavlos.fragkiadoulakis@gmail.com>
2024-01-04 03:27:14 +0200

    Add utility functions for clarity.

--- main.c
+++ main.c
@@ -1,23 +1,39 @@
 #include <windows.h>
-        #include "tchar.h"
+//#include "tchar.h"
 
 const TCHAR *OK_TEXT = TEXT("YEEAAAHHH!");
 const TCHAR *CANCEL_TEXT = TEXT("NOOOOO!");
 
-BOOL CALLBACK MessageBoxEnumProc(HWND hwnd, LPARAM lParam)
+BOOL areStringsEqual(TCHAR *str1, TCHAR *str2)
+{
+    return lstrcmp(str1, str2) == 0;
+}
+
+BOOL isWindowText(HWND hwnd, TCHAR *str)
+{
+    int textLength = GetWindowTextLength(hwnd);
+    TCHAR *text = malloc((textLength + 1) * sizeof(TCHAR));
+    GetWindowText(hwnd, text, textLength + 1);
+    // int length2 = lstrlen(text); //2
+    _tprintf("'%s'\n", text);
+    int result = lstrcmp(text, TEXT("OK")) == 0;
+    free(text);
+    return result == 0;
+}
+
+BOOL isWindowClass(HWND hwnd, TCHAR *str)
 {
     TCHAR className[256];
-    GetClassName(hwnd, className, 256);
-    if (lstrcmp(className, TEXT("Button")) == 0)
-    {
-        //TCHAR text[256];
-        int textLength = GetWindowTextLength(hwnd);
-        TCHAR* text = malloc((textLength+1) * sizeof(TCHAR));
-        GetWindowText(hwnd,text,textLength+1);
-        _tprintf("'%s'\n",text);
-        if(lstrcmp(text,TEXT("OK"))==0) _tprintf(TEXT("FOUND OK!"));
-        free(text);
+    int length = GetClassName(hwnd, className, 256);
+    return lstrcmp(className, str) == 0;
+}
 
+BOOL CALLBACK MessageBoxEnumProc(HWND hwnd, LPARAM lParam)
+{
+    if (isWindowClass(hwnd, TEXT("Button")))
+    {
+        // if (isWindowText(hwnd, TEXT("OK")))
+        //     _tprintf(TEXT("FOUND OK!")); //needs tchar.h
 
         int ctlId = GetDlgCtrlID(hwnd);
         switch (ctlId)
@@ -36,17 +52,9 @@ BOOL CALLBACK MessageBoxEnumProc(HWND hwnd, LPARAM lParam)
 LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
     CWPSTRUCT *msg = (CWPSTRUCT *)lParam;
-    if (msg->message == WM_INITDIALOG)
-    {
-        HWND hwnd = msg->hwnd;
-        TCHAR className[256];
-        GetClassName(hwnd, className, 256);
-
-        // https://learn.microsoft.com/en-us/windows/win32/winmsg/about-window-classes
-        if (lstrcmp(className, TEXT("#32770")) == 0)
-            EnumChildWindows(hwnd, MessageBoxEnumProc, 0);
-    }
-
+    // https://learn.microsoft.com/en-us/windows/win32/winmsg/about-window-classes
+    if (msg->message == WM_INITDIALOG && isWindowClass(msg->hwnd, TEXT("#32770")))
+        EnumChildWindows(msg->hwnd, MessageBoxEnumProc, 0);
     return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
@@ -54,7 +62,7 @@ int main()
 {
     HHOOK hHook = SetWindowsHookEx(WH_CALLWNDPROC, HookProc, NULL, GetCurrentThreadId());
 
-    MessageBox(NULL, "The quantum realm has finally come to an end!", "KEFTEDAKIAS", MB_ICONWARNING | MB_OKCANCEL);
+    MessageBox(NULL,TEXT("The quantum realm has finally come to an end!"), TEXT("KEFTEDAKIAS"), MB_ICONWARNING | MB_OKCANCEL);
 
     UnhookWindowsHookEx(hHook);
 
